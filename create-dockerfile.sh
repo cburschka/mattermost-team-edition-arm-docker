983c61f1deaec25a91bc08f7d2b977803fbdf1e8
Christoph Burschka <christoph@burschka.de>
2026-07-17 18:37:05 +0200

    Make sure to specify the team edition.

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/mattermost/mattermost/refs/tags/v${VERSIO
 wget https://raw.githubusercontent.com/mattermost/mattermost/refs/tags/v${VERSION}/server/build/passwd
 
 sed -i "s|FROM ubuntu:.*|FROM ubuntu:noble AS builder|" Dockerfile
-sed -i "s|ARG MM_PACKAGE=.*|ARG MM_PACKAGE=\"https://releases.mattermost.com/${VERSION}/mattermost-${VERSION}-linux-arm64.tar.gz?src=docker\"|" Dockerfile
+sed -i "s|ARG MM_PACKAGE=.*|ARG MM_PACKAGE=\"https://releases.mattermost.com/${VERSION}/mattermost-team-${VERSION}-linux-arm64.tar.gz?src=docker\"|" Dockerfile
